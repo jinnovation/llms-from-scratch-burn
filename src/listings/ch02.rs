e72d99709c1537218964d9508d6c175172c1377e
Jonathan Jin <me@jonathanj.in>
2025-12-24 11:37:12 -0500

    expand test

--- src/listings/ch02.rs
+++ src/listings/ch02.rs
@@ -61,6 +61,11 @@ impl Listing for L2_1 {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        fs::{self, File},
+        io,
+    };
+
     use crate::listings::ch02::tokenize;
 
     #[test]
@@ -70,6 +75,31 @@ mod tests {
             Vec::from([
                 "Hello", ",", "world", ".", "Is", "this", "--", "a", "test", "?"
             ])
+        )
+    }
+
+    #[test]
+    fn test_tokenize_short_story() {
+        let client = reqwest::blocking::Client::new();
+
+        let path = "/tmp/the-verdict.txt";
+
+        let mut res = client.get(
+            "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt",
+        ).send().unwrap().error_for_status().unwrap();
+        let mut file = File::create(path).unwrap();
+
+        io::copy(&mut res, &mut file).unwrap();
+
+        let opened = fs::read_to_string(path).unwrap();
+
+        assert_eq!(
+            tokenize(&opened)[0..30],
+            [
+                "I", "HAD", "always", "thought", "Jack", "Gisburn", "rather", "a", "cheap",
+                "genius", "--", "though", "a", "good", "fellow", "enough", "--", "so", "it", "was",
+                "no", "great", "surprise", "to", "me", "to", "hear", "that", ",", "in"
+            ]
         )
     }
 }
