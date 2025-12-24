fad1d53e0fa36b9e361f3647e2f593a0199b1f26
Jonathan Jin <me@jonathanj.in>
2025-12-24 10:48:40 -0500

    log count + excerpt

--- src/listings/ch02.rs
+++ src/listings/ch02.rs
@@ -1,4 +1,8 @@
-use std::{error::Error, fs::File, io};
+use std::{
+    error::Error,
+    fs::{self, File},
+    io,
+};
 
 use log::info;
 
@@ -10,14 +14,20 @@ impl Listing for L2_1 {
     fn main(&self) -> Result<(), Box<dyn Error>> {
         let client = reqwest::blocking::Client::new();
 
+        let path = "/tmp/the-verdict.txt";
+
         let mut res = client.get(
             "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt",
         ).send()?.error_for_status()?;
-        let mut file = File::create("/tmp/the-verdict.txt")?;
+        let mut file = File::create(path)?;
 
-        info!(path = "tmp/the-verdict.txt"; "downloaded file");
+        info!(path; "downloaded file");
 
         io::copy(&mut res, &mut file)?;
+
+        let opened = fs::read_to_string(path)?;
+        info!(count = opened.chars().count(), excerpt:? = opened[0..99]; "file details");
+
         Ok(())
     }
 }
