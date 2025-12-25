a7682ca849906b58279fb6f920ee1182244d8b18
Jonathan Jin <me@jonathanj.in>
2025-12-25 15:51:07 -0500

    chapter 2: construct vocab

--- src/listings/ch02.rs
+++ src/listings/ch02.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::{HashMap, HashSet},
     error::Error,
     fs::{self, File},
     io,
@@ -12,7 +13,7 @@ use crate::Listing;
 pub struct L2_1;
 
 fn tokenize(s: &str) -> Vec<&str> {
-    let regex = Regex::new(r#"([,.:;?_!"()\"]|--|\s)"#).unwrap();
+    let regex = Regex::new(r#"([,.:;?_!"()\']|--|\s)"#).unwrap();
     let mut result = Vec::new();
     let mut last_end = 0;
 
@@ -34,6 +35,27 @@ fn tokenize(s: &str) -> Vec<&str> {
         .collect()
 }
 
+fn uniq(strings: Vec<&str>) -> Vec<String> {
+    strings
+        .iter()
+        .cloned()
+        .map(|str| str.to_string())
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect()
+}
+
+fn construct_vocab(corpus: &String) -> HashMap<String, usize> {
+    let mut all_words = uniq(tokenize(&corpus));
+    all_words.sort();
+
+    all_words.iter().fold(HashMap::new(), |mut acc, word| {
+        let count = acc.entry(word.to_string()).or_insert(0);
+        *count += 1;
+        acc
+    })
+}
+
 impl Listing for L2_1 {
     fn main(&self) -> Result<(), Box<dyn Error>> {
         let client = reqwest::blocking::Client::new();
@@ -52,8 +74,9 @@ impl Listing for L2_1 {
         let opened = fs::read_to_string(path)?;
         info!(count = opened.chars().count(), excerpt:? = opened[0..99]; "file details");
 
-        let tokenized = tokenize("Hello, world. Is this-- a test?");
-        info!(tokenized:? = tokenized; "tokenized");
+        let vocab = construct_vocab(&opened);
+
+        info!(vocab_size = vocab.len(); "collected vocabulary");
 
         Ok(())
     }
@@ -66,7 +89,7 @@ mod tests {
         io,
     };
 
-    use crate::listings::ch02::tokenize;
+    use crate::listings::ch02::{tokenize, uniq};
 
     #[test]
     fn test_tokenize() {
@@ -93,13 +116,19 @@ mod tests {
 
         let opened = fs::read_to_string(path).unwrap();
 
+        let tokenized = tokenize(&opened);
+
         assert_eq!(
-            tokenize(&opened)[0..30],
+            tokenized[0..30],
             [
                 "I", "HAD", "always", "thought", "Jack", "Gisburn", "rather", "a", "cheap",
                 "genius", "--", "though", "a", "good", "fellow", "enough", "--", "so", "it", "was",
                 "no", "great", "surprise", "to", "me", "to", "hear", "that", ",", "in"
             ]
-        )
+        );
+
+        let vocab = uniq(tokenized);
+
+        assert_eq!(vocab.len(), 1130);
     }
 }
