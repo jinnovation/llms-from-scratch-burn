dbc7e12842f662ddcb1598b818352053c8529f7d
Jonathan Jin <me@jonathanj.in>
2025-12-26 10:48:03 -0500

    implement SimpleTokenizerV2

--- src/listings/ch02.rs
+++ src/listings/ch02.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::{
     collections::{HashMap, HashSet},
     error::Error,
@@ -46,9 +47,15 @@ fn uniq(strings: Vec<&str>) -> Vec<String> {
         .collect()
 }
 
-fn construct_vocab(corpus: &String) -> HashMap<String, usize> {
+fn construct_vocab(
+    corpus: &String,
+    additional_tokens: Option<Vec<String>>,
+) -> HashMap<String, usize> {
     let mut all_words = uniq(tokenize(&corpus));
     all_words.sort();
+    if let Some(tokens) = additional_tokens {
+        all_words.extend(tokens.iter().map(|t| t.to_string()));
+    }
 
     HashMap::from_iter(
         all_words
@@ -58,7 +65,10 @@ fn construct_vocab(corpus: &String) -> HashMap<String, usize> {
     )
 }
 
-fn construct_vocab_from_url(corpus_url: String) -> Result<HashMap<String, usize>, Box<dyn Error>> {
+fn construct_vocab_from_url(
+    corpus_url: String,
+    additional_tokens: Option<Vec<String>>,
+) -> Result<HashMap<String, usize>, Box<dyn Error>> {
     let client = reqwest::blocking::Client::new();
 
     let mut res = client.get(corpus_url).send()?.error_for_status()?;
@@ -70,7 +80,7 @@ fn construct_vocab_from_url(corpus_url: String) -> Result<HashMap<String, usize>
     let opened = fs::read_to_string(file.path())?;
     info!(count = opened.chars().count(), excerpt:? = opened[0..99]; "file details");
 
-    Ok(construct_vocab(&opened))
+    Ok(construct_vocab(&opened, additional_tokens))
 }
 
 impl Listing for L2_1 {
@@ -91,7 +101,7 @@ impl Listing for L2_1 {
         let opened = fs::read_to_string(path)?;
         info!(count = opened.chars().count(), excerpt:? = opened[0..99]; "file details");
 
-        let vocab = construct_vocab(&opened);
+        let vocab = construct_vocab(&opened, None);
 
         info!(vocab_size = vocab.len(); "collected vocabulary");
 
@@ -111,7 +121,7 @@ struct SimpleTokenizerV1 {
 
 impl SimpleTokenizerV1 {
     fn new(vocab: HashMap<String, usize>) -> Self {
-        SimpleTokenizerV1 {
+        Self {
             str_to_int: vocab.clone(),
             int_to_str: vocab.iter().map(|(k, v)| (*v, k.clone())).collect(),
         }
@@ -136,6 +146,53 @@ impl Tokenizer for SimpleTokenizerV1 {
     }
 }
 
+struct SimpleTokenizerV2 {
+    str_to_int: HashMap<String, usize>,
+    int_to_str: HashMap<usize, String>,
+}
+
+enum Corpus {
+    Raw(String),
+    URL(String),
+}
+
+impl SimpleTokenizerV2 {
+    fn new(corpus: Corpus) -> Self {
+        let additional_tokens = vec!["<|endoftext|>".to_string(), "<|unk|>".to_string()];
+        let vocab = match corpus {
+            Corpus::Raw(text) => construct_vocab(&text, additional_tokens.into()),
+            Corpus::URL(url) => construct_vocab_from_url(url, additional_tokens.into()).unwrap(),
+        };
+        Self {
+            str_to_int: vocab.clone(),
+            int_to_str: vocab.iter().map(|(k, v)| (*v, k.clone())).collect(),
+        }
+    }
+}
+
+impl Tokenizer for SimpleTokenizerV2 {
+    fn encode(&self, text: String) -> Vec<usize> {
+        tokenize(&text)
+            .iter()
+            .map(|token| {
+                *(self
+                    .str_to_int
+                    .get(*token)
+                    .unwrap_or(&self.str_to_int["<|unk|>"]))
+            })
+            .collect()
+    }
+
+    fn decode(&self, ids: Vec<usize>) -> String {
+        let tokens: Vec<String> = ids.iter().map(|id| self.int_to_str[id].clone()).collect();
+        let joined = tokens.join(" ");
+        let regex = Regex::new(r#"\s+([,.?!"()'])"#).unwrap();
+        let replaced = regex.replace_all(&joined, "$1").to_string();
+
+        replaced
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{
@@ -143,12 +200,53 @@ mod tests {
         io,
     };
 
-    use crate::listings::ch02::{SimpleTokenizerV1, Tokenizer, construct_vocab_from_url, tokenize};
+    use crate::listings::ch02::{
+        Corpus, SimpleTokenizerV1, SimpleTokenizerV2, Tokenizer, construct_vocab_from_url, tokenize,
+    };
 
     #[test]
-    fn test_simple_tokenizer_tokenize() {
+    fn test_simple_tokenizer_v2_special_tokens() {
+        let tokenizer = SimpleTokenizerV2::new(Corpus::URL(
+            "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt".to_string(),
+        ));
+
+        assert_eq!(tokenizer.str_to_int["younger"], 1127);
+        assert_eq!(tokenizer.str_to_int["your"], 1128);
+        assert_eq!(tokenizer.str_to_int["yourself"], 1129);
+        assert_eq!(tokenizer.str_to_int["<|endoftext|>"], 1130);
+        assert_eq!(tokenizer.str_to_int["<|unk|>"], 1131);
+    }
+
+    #[test]
+    fn test_simple_tokenizer_v2_roundtrip() {
+        let tokenizer = SimpleTokenizerV2::new(Corpus::URL(
+            "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt".to_string(),
+        ));
+
+        let input = "Hello, do you like tea? <|endoftext|> In the sunlit terraces of the palace.";
+
+        let encoded = tokenizer.encode(input.to_string());
+
+        assert_eq!(
+            encoded,
+            [
+                1131, 5, 355, 1126, 628, 975, 10, 1130, 55, 988, 956, 984, 722, 988, 1131, 7
+            ],
+        );
+
+        let decoded = tokenizer.decode(encoded);
+
+        assert_eq!(
+            decoded,
+            "<|unk|>, do you like tea? <|endoftext|> In the sunlit terraces of the <|unk|>."
+        )
+    }
+
+    #[test]
+    fn test_simple_tokenizer_v1_tokenize() {
         let tokenizer = SimpleTokenizerV1::new(construct_vocab_from_url(
             "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt".to_string(),
+            None,
         ).unwrap());
 
         let input =
@@ -203,9 +301,22 @@ mod tests {
     }
 
     #[test]
+    fn test_tokenize_additional_tokens() {
+        let vocab = construct_vocab_from_url(
+
+            "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt".to_string(),
+            vec!["<|endoftext|>".to_string(), "<|unk|>".to_string()].into(),
+        ).unwrap();
+
+        assert_eq!(vocab["<|endoftext|>"], 1130);
+        assert_eq!(vocab["<|unk|>"], 1131);
+    }
+
+    #[test]
     fn test_construct_vocab() {
         let vocab = construct_vocab_from_url(
             "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt".to_string(),
+            None,
         ).unwrap();
 
         assert_eq!(vocab.len(), 1130);
